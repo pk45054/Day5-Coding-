b6b9df34c350dbbfb19054ac347db1be0c88961b
Raj Kumar Karki <84299281+pk45054@users.noreply.github.com>
2021-06-25 10:47:28 +0545

    Add files via upload

---
+++ CreateObject.java
@@ -0,0 +1,13 @@
+class CreateObject 
+{  
+//constructor of the class    
+CreateObject()    
+{    
+System.out.println("Namaste Raj Kumar");    
+}    
+public static void main(String[] args)   
+{    
+//creating an object using new keyword   
+CreateObject obj = new CreateObject();   
+}    
+}   
